681d7742fd1b1653c2d9b27041974a9fefb300ab
Nitin <nk17oct@gmail.com>
2023-04-03 23:52:17 +0530

    decrease stock on order

--- server/routes/order.js
+++ server/routes/order.js
@@ -15,15 +15,21 @@ router.route('/')
 })
 .post(async(req, res) => {
     try{
+        // get cart items
         const items = await db.query('SELECT * FROM cart JOIN (SELECT id, price FROM product) t ON t.id=product_id WHERE customer_id = $1', [1]);
         if(!items.rows.length){
             res.status(403).send("No items in cart");
         }
 
+        // make order
         const order = await db.query('INSERT INTO orders(customer_id, date) VALUES($1, $2) RETURNING id', [1, new Date()]);
         
+        
         items.rows.forEach(async(item)=>{
+            // insert order items
             await db.query('INSERT INTO order_items(order_id, product_id, quantity, price) VALUES($1, $2, $3, $4)', [order.rows[0].id, item.product_id, item.quantity, item.price*item.quantity]);
+            // decrese product stock
+            await db.query('UPDATE product SET stock = stock - $1 where id = $2', [item.quantity, item.product_id]);
         });
 
         await db.query('DELETE FROM cart WHERE customer_id = $1', [1]);
